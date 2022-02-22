ce252e3bbc7c4750ea9b8ebc1fecf11526311e1b
michal-cesky <99603937+michal-cesky@users.noreply.github.com>
2022-02-22 12:55:43 +0100

    Update Matrix.cs

--- Cvičení 3 Matice/ConsoleApp1/Matrix.cs
+++ Cvičení 3 Matice/ConsoleApp1/Matrix.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,120 +9,186 @@ namespace Cviceni_3
 {
     public class Matrix
     {
-        public double[,] Matice;
+        public double[,] matrix1;
 
 
         public Matrix(double[,] matrix)
         {
-            Matice = matrix;
+            matrix1 = matrix;
 
         }
 
-        public static Matrix operator +(Matrix a, Matrix b) 
+        public static Matrix operator +(Matrix a, Matrix b)
         {
             try
             {
-              
 
-                var mtx = new Matrix(new double[a.Matice.GetLength(0), a.Matice.GetLength(1)]);          //0 sloiupec, 1 řádek
-                for (int i = 0; i < a.Matice.GetLength(0); i++)
+
+                var mtx = new Matrix(new double[a.matrix1.GetLength(0), a.matrix1.GetLength(1)]);          //0 sloiupec, 1 řádek
+                for (int i = 0; i < a.matrix1.GetLength(0); i++)
                 {
-                    for (int j = 0; j < a.Matice.GetLength(1); j++)
+                    for (int j = 0; j < a.matrix1.GetLength(1); j++)
                     {
-                        mtx.Matice[i, j] = a.Matice[i, j] + b.Matice[i, j];
+                        mtx.matrix1[i, j] = a.matrix1[i, j] + b.matrix1[i, j];
                     }
                 }
                 return mtx;
             }
             catch
             {
-                Console.WriteLine("MAtice nebylo mozno secist");
+                throw new InvalidCastException();
 
             }
 
-            return a;
+           
         }
 
-        public static Matrix operator -(Matrix a, Matrix b) 
+        public static Matrix operator -(Matrix a, Matrix b)
         {
             try
             {
-                var mtx = new Matrix(new double[a.Matice.GetLength(0), a.Matice.GetLength(1)]);          //0 sloiupec, 1 řádek
-                for (int i = 0; i < a.Matice.GetLength(0); i++)
+                var mtx = new Matrix(new double[a.matrix1.GetLength(0), a.matrix1.GetLength(1)]);          //0 sloiupec, 1 řádek
+                for (int i = 0; i < a.matrix1.GetLength(0); i++)
                 {
-                    for (int j = 0; j < a.Matice.GetLength(1); j++)
+                    for (int j = 0; j < a.matrix1.GetLength(1); j++)
                     {
-                        mtx.Matice[i, j] = a.Matice[i, j] - b.Matice[i, j];
+                        mtx.matrix1[i, j] = a.matrix1[i, j] - b.matrix1[i, j];
                     }
                 }
                 return mtx;
             }
             catch
             {
-                Console.WriteLine("Matice nebylo mozno odecist");
+                throw new InvalidCastException();
 
             }
 
-            return a;
+            
         }
 
-        public static Matrix operator *(Matrix a, Matrix b) 
+        public static Matrix operator *(Matrix a, Matrix b)
         {
             try
             {
-                var mtx = new Matrix(new double[a.Matice.GetLength(0), a.Matice.GetLength(1)]);          //0 sloiupec, 1 řádek
-                for (int i = 0; i < a.Matice.GetLength(0); i++)
+                var mtx = new Matrix(new double[a.matrix1.GetLength(0), a.matrix1.GetLength(1)]);          //0 sloiupec, 1 řádek
+                for (int i = 0; i < a.matrix1.GetLength(0); i++)
                 {
-                    for (int j = 0; j < a.Matice.GetLength(1); j++)
+                    for (int j = 0; j < a.matrix1.GetLength(1); j++)
                     {
-                            mtx.Matice[i, j] = 0;
-                            for (int k = 0; k < a.Matice.GetLength(1); k++) 
-                                mtx.Matice[i, j] = mtx.Matice[i, j] + a.Matice[i, k] * b.Matice[k, j];
+                        mtx.matrix1[i, j] = 0;
+                        for (int k = 0; k < a.matrix1.GetLength(1); k++)
+                            mtx.matrix1[i, j] = mtx.matrix1[i, j] + a.matrix1[i, k] * b.matrix1[k, j];
+                    }
+
+                }
+                return mtx;
+
+            }
+            catch
+            {
+                throw new InvalidCastException();
+
+            }
+
+           
+        }
+        public static bool operator ==(Matrix a, Matrix b)
+        {
+            for (int i = 0; i < a.matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.matrix1.GetLength(1); j++)
+                {
+                    if (a.matrix1[i, j] == b.matrix1[i, j])
+                    {
+                        return true;
+                    }
+
+                }
+
+            }
+            return false;
+
+
+        }
+
+        public static bool operator !=(Matrix a, Matrix b)
+        {
+
+                for (int i = 0; i < a.matrix1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.matrix1.GetLength(1); j++)
+                    {
+                        if (a.matrix1[i, j] != b.matrix1[i, j])
+                        {
+                        return false;
                         }
 
                     }
-                    return mtx;
-                
-            }
-            catch
+                    
+                }
+            return true;
+   
+
+
+        }
+
+        public static Matrix Associated(Matrix a)
+        {
+
+            var mtx = new Matrix(new double[a.matrix1.GetLength(0), a.matrix1.GetLength(1)]);
+            for (int i = 0; i < a.matrix1.GetLength(0); i++)
             {
-                Console.WriteLine("Matice nebylo mzne vynasobit");
+                for (int j = 0; j < a.matrix1.GetLength(1); j++)
+                {
+                    mtx.matrix1[i, j] = a.matrix1[i, j] * (-1);
+
+                }
+
 
             }
+            return mtx;
 
-            return a;
         }
-    
-       /* public static bool operator ==(Matrix a, Matrix b)
+
+
+        public double Determinant()
         {
-            return a == b;
+            if (matrix1.GetLength(0) == matrix1.GetLength(1) && matrix1.GetLength(1) == 1)
+            {
+                return matrix1[0, 0];
+
+            }
+            else if (matrix1.GetLength(0) == matrix1.GetLength(1) && matrix1.GetLength(1) == 2)
+            {
+                return (matrix1[0, 0] * matrix1[1, 1]) - (matrix1[0, 1] * matrix1[1, 0]);
+
+            }
+            else if (matrix1.GetLength(0) == matrix1.GetLength(1) && matrix1.GetLength(1) == 3)
+            {
+                return matrix1[0, 0] * (matrix1[1, 1] * matrix1[2, 2] - matrix1[1, 2] * matrix1[2, 1]) -
+               matrix1[0, 1] * (matrix1[1, 0] * matrix1[2, 2] - matrix1[1, 2] * matrix1[2, 0]) +
+                matrix1[0, 2] * (matrix1[1, 0] * matrix1[2, 1] - matrix1[1, 1] * matrix1[2, 0]);
+            }
+            else
+            {
+                throw new InvalidCastException();
+            }
+
         }
-        public static bool operator !=(Matrix a, Matrix b) //Pro operator == musim definovat take !=
-        {
-            return !(a == b); //Uz mam definovano v opeeratoru ==
-        }
-        public static Matrix operator -(Matrix a) //Unarni operator -
-        {
-            return new Matrix();
-        }
-        public Matrix Determinant() //Vraci determinant matice
-        {
-            return new Matrix();
-        }*/
 
 
         public override string ToString() //Vypis do retezce
         {
-            var row = Matice.GetLength(0); 
+            var row = matrix1.GetLength(0);
 
-            var columm = Matice.GetLength(1);
-             
+            var columm = matrix1.GetLength(1);
+
             string output = "";
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < columm; j++)
                 {
-                    if (j != row ) output += $"{Matice[i, j]} ";
+                    if (j != row) output += $"{matrix1[i, j]}\t ";
                 }
                 output += Environment.NewLine;
             }
