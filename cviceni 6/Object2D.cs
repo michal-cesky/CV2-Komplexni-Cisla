b2e6a7c69e92c1ff3c1f43400744e6dfd5e2e0ba
Michal Český <xcesky00@vutbr.cz>
2022-03-14 21:28:23 +0100

    1

---
+++ cviceni 6/Object2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_cv6
+{
+    public abstract class Object2D : GrObject
+    {
+        public abstract double CalculateArea();
+    }
+}
